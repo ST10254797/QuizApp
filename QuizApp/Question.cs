7e21cac9f8f6781ac9d9de23109b04e584275d0d
ST10254797 <cristianoronaldonaidoo@gmail.com>
2025-01-16 09:20:58 +0200

    Added code to the Question. cs

--- QuizApp/Question.cs
+++ QuizApp/Question.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace QuizApp
 {
-    internal class Question
+    public class Question
     {
+        public string QuestionText { get; set; } // The text of the question
+        public string QuestionType { get; set; } // "MCQ" for Multiple Choice, "TF" for True/False
+        public string[] Options { get; set; }    // The options for MCQs
+        public string CorrectAnswer { get; set; } // The correct answer for the question
     }
 }
